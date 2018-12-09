cc137bdc72f1f310b363d1d9535522402ff517de
Ankit Anand <ankit.anand@oracle.com>
2018-12-09 15:49:38 +0530

    updated manifest json

--- static/installPrompt.js
+++ static/installPrompt.js
@@ -14,7 +14,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
 function addClickHandler(btnAdd){
   btnAdd.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
-    btnAdd.style.display = 'none';
+    btnAdd.style.display = 'inline';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
