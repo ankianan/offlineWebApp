a58bbce7be60a3d22febec0685c179a70ae8dd69
Ankit Anand <ankit.anand@oracle.com>
2018-12-09 16:05:38 +0530

    changed display block to inline in install prompt

--- static/installPrompt.js
+++ static/installPrompt.js
@@ -8,13 +8,13 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Update UI notify the user they can add to home screen
   let btnA2HS = document.getElementById("A2HS");
   addClickHandler(btnA2HS);
-  btnA2HS.style.display = 'block';
+  btnA2HS.style.display = 'inline';
 });
 
 function addClickHandler(btnAdd){
   btnAdd.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
-    btnAdd.style.display = 'inline';
+    btnAdd.style.display = 'none';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
